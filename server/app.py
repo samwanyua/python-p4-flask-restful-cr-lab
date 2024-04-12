5e3b988a619ab6a18bf50bdd720999de9a5dafa6
samwanyua <samexample8@gmail.com>
2024-04-12 20:53:59 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -17,11 +17,62 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+
+    def get(self):
+        plants_dict_list = [plant.to_dict() for plant in Plant.query.all()]
+        response = make_response(
+            plants_dict_list,
+            200,
+        )
+        return response
+
+
+    def post(self):
+
+        data = request.get_json()
+
+        new_plant = Plant(
+            name = data['name'],
+            image = data['image'],
+            price = data['price'],
+        )
+
+        db.session.add(new_plant)
+
+        db.session.commit()
+
+        response = make_response(
+            new_plant,
+            201,
+        )
+        return response
+
+api.add_resource(Plants, '/plants')
+
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+
+        plant = Plant.query.filter(Plant.id == id).first()
+
+        if plant == None:
+
+            response_body = {
+                "message": "This record does not exist in our database. Please try again."
+            }
+            response = make_response(response_body, 404)
+            return response
+
+        else:
+
+            plant_dict = plant.to_dict()
+            response = make_response(
+                plant_dict,
+                200
+            )
+            return response
+
+api.add_resource(PlantByID, '/plants/<int:id>')	        
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
